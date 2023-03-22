b93fd929f9cb8fd86a0871d7ac6a6063cb8e4d93
ChangayaMusic <yroker@gmail.com>
2023-03-22 10:25:49 +0100

    add title and link

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
 from csv import DictWriter
-
 from requests import get
 from bs4 import BeautifulSoup
 
+url = 'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
+
 def get_book_data(url):
+
+    print(url)
     response = get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     table = soup.find('table')
     rows = table.find_all('tr')
-    informations = {}
+    titre = soup.find('h1').text
+
+    informations = {titre: url}
+
     for row in rows:
         informations[row.find('th').text] = row.find('td').text
     print(informations)
     return informations
 
 if __name__ == '__main__':
-    data = get_book_data("http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html")
-    with open("informations.csv", "w", newline="") as csv_file:
+
+    data = get_book_data(url)
+    with open('informations.csv', "w", newline="") as csvfile:
         fieldnames = data.keys()
-        writer = DictWriter(csv_file, fieldnames=fieldnames)
+        writer = DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
         writer.writerow(data)
