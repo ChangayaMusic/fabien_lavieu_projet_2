aa2b89e33cdf32716012d5428c0027b599d81a88
ChangayaMusic <yroker@gmail.com>
2023-03-22 18:44:36 +0100

    test url pages by next buttonworking

--- cat.py
+++ cat.py
@@ -1,16 +1,48 @@
 from csv import DictWriter
 from requests import get
 from bs4 import BeautifulSoup
-from math import ceil
 
-url_c = 'http://books.toscrape.com/catalogue/category/books_1/index.html'
-print(url_c)
-response_c = get(url_c)
-print(response_c)
-soup_c = BeautifulSoup(response_c.text, 'html.parser')
-quantity = soup_c.find(class_="form-horizontal").text
-print(quantity)
-quantity_nb= quantity[3:5]
-print(quantity_nb)
-page_nb = ceil((int(quantity_nb)/20))
-print(page_nb)
+url = 'http://books.toscrape.com/catalogue/category/books/sequential-art_5/'
+
+def get_book_data(url):
+
+    print(url)
+    response = get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    table = soup.find('table')
+    rows = table.find_all('tr')
+    titre = soup.find('h1').text
+
+    informations = {titre: url}
+
+    for row in rows:
+        informations[row.find('th').text] = row.find('td').text
+    print(informations)
+    return informations
+
+#if __name__ == '__main__':
+def csv():
+    data = get_book_data(url)
+    with open('informations.csv', "w", newline="") as csvfile:
+        fieldnames = data.keys()
+        writer = DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerow(data)
+
+def nextpage():
+    url_a = ""
+    url_c = url + url_a
+    print(url_c)
+    response_c = get(url_c)
+    print(response_c)
+    soup_c = BeautifulSoup(response_c.content, 'html.parser')
+    quantity = soup_c.find(class_="next")
+    print(type(quantity))
+    next_page = quantity.find('a').get("href")
+    print(quantity)
+    print(next_page)
+    url_ = next_page
+
+
+
+nextpage()
