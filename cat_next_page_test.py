bec6a9b05e4492950bbb179342cc5dc25c73daf1
ChangayaMusic <yroker@gmail.com>
2023-03-22 18:40:35 +0100

    test url pages by next buttonworking

--- cat_next_page_test.py
+++ cat_next_page_test.py
@@ -2,20 +2,23 @@ from csv import DictWriter
 from requests import get
 from bs4 import BeautifulSoup
 
-url_c = 'http://books.toscrape.com/catalogue/category/books_1/index.html'
-
-def next():
+url = 'http://books.toscrape.com/catalogue/category/books/sequential-art_5/'
 
 
-    print(url_c)
-    response_c = get(url_c)
-    print(response_c)
-    soup_c = BeautifulSoup(response_c.text, 'html.parser')
-    quantity = soup_c.find(class_="next")
-    next_page = quantity.find('a').get("href")
-    print(quantity)
-    print(next_page)
-    next_url = url_c.replace("index.html", next_page)
-    print(next_url)
+
+response = requests.get(url)
+print(response)
+
+
+
+
+
+
+
+
 next()
+
+
+
+
 
