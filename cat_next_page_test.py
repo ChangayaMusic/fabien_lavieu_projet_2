7efbc7d54eedf9b71ba3db6c0251d764e646a46d
ChangayaMusic <yroker@gmail.com>
2023-03-23 10:47:30 +0100

    All Working + create categroy url list

--- cat_next_page_test.py
+++ cat_next_page_test.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
+from csv import DictWriter
 
 url = 'http://books.toscrape.com/catalogue/category/books/sequential-art_5/'
 
@@ -12,10 +13,8 @@ while True:
     footer = soup.select_one('li.current')
     print(footer.text.strip())
     next_page = soup.select_one('li.next>a')
-    print(next_page)
     if next_page:
         next_url = next_page.get('href')
-        print(next_url)
         url = urljoin(url, next_url)
         print(url)
     else:
