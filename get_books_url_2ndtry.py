8dd2828ee1bfbd1f8f20cf4c7f24b3c550e808c7
ChangayaMusic <yroker@gmail.com>
2023-03-23 12:38:40 +0100

    All Working + create categroy url list

---
+++ get_books_url_2ndtry.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'http://books.toscrape.com/catalogue/category/books/mystery_3/index.html'
+response = requests.get(url)
+soup = BeautifulSoup(response.content, 'html.parser')
+h3_tags = soup.find_all('h3')
+product_links = []
+for h3 in h3_tags:
+    href = h3.a['href']
+    product_link = 'http://books.toscrape.com/catalogue/' + href
+    product_links.append(product_link)
+    print(product_link)
