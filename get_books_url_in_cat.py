3635355855d7325d14fbde8c14a1fe5271754e12
ChangayaMusic <yroker@gmail.com>
2023-03-23 15:22:21 +0100

    get all titles but table info not working

--- get_books_url_in_cat.py
+++ get_books_url_in_cat.py
@@ -1,18 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
+from csv import DictWriter
 
-url = 'http://books.toscrape.com/catalogue/category/books/mystery_3/index.html'
+url = 'http://books.toscrape.com/catalogue/category/books/mystery_3/'
 response = requests.get(url)
 soup = BeautifulSoup(response.content, 'html.parser')
-h3_tags = soup.find_all('h3')
 product_links = []
-for h3 in h3_tags:
-    href = h3.a['href']
-    product_link = 'http://books.toscrape.com/catalogue/' + href
-    product_links.append(product_link)
-    print(product_link)
 
+def get_all_products_links():
+    h3_tags = soup.find_all('h3')
+    print(h3_tags)
 
+    for h3 in h3_tags:
+        href = h3.a['href']
+        print(href)
+        product_link = url + href
+        product_links.append(product_link)
+        print(product_link)
+
+get_all_products_links()
+print(product_links)
 
 
 
