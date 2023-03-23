614babfa730753bb27c92777c80aef823fb42dde
ChangayaMusic <yroker@gmail.com>
2023-03-23 14:15:56 +0100

    access all pages of category and get all books urls

--- cat_next_page_test.py
+++ cat_next_page_test.py
@@ -1,12 +1,40 @@
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
-from csv import DictWriter
 
 url = 'http://books.toscrape.com/catalogue/category/books/sequential-art_5/'
+next_page_url = []
+product_links = []
+def get_book_data():
+
+    print(url)
+
+
+
+#if __name__ == '__main__':
+def csv():
+    data = get_book_data(url)
+    with open('informations.csv', "w", newline="") as csvfile:
+        fieldnames = data.keys()
+        writer = DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerow(data)
+
+
+
+def get_all_products_links():
+    h3_tags = soup.find_all('h3')
+    print(h3_tags)
+
+    for h3 in h3_tags:
+        href = h3.a['href']
+        print(href)
+        product_link = url + href
+        product_links.append(product_link)
+        print(product_link)
 
 while True:
-
+    next_page_url.append(url)
     response = requests.get(url)
     print(response)
     soup = BeautifulSoup(response.content, "html.parser")
@@ -19,8 +47,16 @@ while True:
         print(url)
     else:
         break
+print(next_page_url)
 
+for i in next_page_url:
+    url = i
+    get_all_products_links()
+print(product_links)
 
+for bk in product_links:
+    url = bk
+    get_book_data()
 
 
 
