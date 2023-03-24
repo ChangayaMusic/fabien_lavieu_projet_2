93e70abbc03e9a2c302bf99e902dc2b9378fa8cb
ChangayaMusic <yroker@gmail.com>
2023-03-24 21:58:28 +0100

    working !!!

--- new_code_get_info.py
+++ new_code_get_info.py
@@ -1,16 +1,17 @@
+import csv
+
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
-import csv
 from csv import DictWriter
-
-url = 'http://books.toscrape.com/catalogue/category/books/mystery_3/'
 next_page_url = []
 product_links = []
 informations_list = []
-info_list = []
 links_words = []
-def get_book_data():
+
+url = "http://books.toscrape.com/catalogue/1st-to-die-womens-murder-club-1_2/index.html"
+
+def get_book_data_to_csv():
 
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
@@ -75,68 +76,17 @@ def get_book_data():
     #print(review_rating)
     #print(img_url)
 
-def get_all_products_links():
-    h3_tags = soup.find_all('h3')
-    print(h3_tags)
-
-    for h3 in h3_tags:
-        href = h3.a['href']
-        href = href[9:]
-        print(href)
-        product_link = "http://books.toscrape.com/catalogue/" + href
-        product_links.append(product_link)
-        print(product_link)
-
-while True:
-    next_page_url.append(url)
-    response = requests.get(url)
-    print(response)
-    soup = BeautifulSoup(response.content, "html.parser")
-    footer = soup.select_one('li.current')
-    print(footer.text.strip())
-    next_page = soup.select_one('li.next>a')
-    if next_page:
-        next_url = next_page.get('href')
-        print(next_url)
-        url = urljoin(url, next_url)
-        print(url)
-    else:
-        break
-print(next_page_url)
-
-for i in next_page_url:
-    url = i
-    get_all_products_links()
-print(product_links)
-
-for p in product_links:
-    url = p
+with open("v3.csv", "w", newline="") as csv_file:
     get_book_data()
-
-with open("v3.csv", "w", newline="", encoding="utf-8") as csv_file:
-
-
     print(informations_list)
     header = ['Title', 'Product_page_url', 'Review_rating', 'Product_description', 'image_url', 'UPC',
               'Product Type', 'Price (excl. tax)', 'Price (incl. tax)', 'Tax', 'Availability',
               'Number of reviews']
 
+
     writer = csv.writer(csv_file)
     writer.writerow(header)
-    writer.writerows(informations_list)
-
-
-
-
-
-
-
-
-
-
-
-
-
+    writer.writerow(informations_list)
 
 
 
