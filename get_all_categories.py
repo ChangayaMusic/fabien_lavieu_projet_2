e81b4b68d4502a117da11ce68d37eb438274dc33
ChangayaMusic <yroker@gmail.com>
2023-03-25 12:27:42 +0100

    all working category_links_list = [] category_name_list = [] category_dict = {}

--- get_all_categories.py
+++ get_all_categories.py
@@ -1,24 +1,38 @@
 import requests
 from bs4 import BeautifulSoup
-category_links = []
-category_list = []
-url = 'http://books.toscrape.com/'
+
+
+category_links_list = []
 category_name_list = []
+category_dict = {}
+
+url = 'http://books.toscrape.com/'
+
 response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
-h3_tags = soup.find(class_="nav nav-list")
-h = h3_tags.find_all('a')
-print(h)
-for e in h:
-    #print(e)
-    hr = e.text.replace('\n', '').strip()
-    category_list.append(hr)
-    print(hr)
-category_list = category_list[1:51]
-print(len(category_list))
-print(("------------------------------------------------------"))
-print(category_list)
+nav_tags = soup.find(class_="nav nav-list")
+nav = nav_tags.find_all('a')
+#print(nav)
 
+for e in nav:
+    #print(e)
+    nav_text = e.text.replace('\n', '').strip()
+    category_name_list.append(nav_text)
+
+    #print(h_text)
+    link_category = e['href']
+    category_links_list.append(url+link_category)
+    #print(link_category)
+category_links_list = category_links_list[1:51]
+category_name_list = category_name_list[1:51]
+print(category_links_list)
+
+category_dict= dict(zip(category_name_list,category_links_list))
+
+#print(category_name_list)
+#print(("------------------------------------------------------"))
+
+print(category_dict)
 
 
 
